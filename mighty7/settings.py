2b53df7d5f41a1d684a9450c195af2c166c7e8b7
14kkeun <14.kkeun@gmail.com>
2024-08-12 17:47:43 +0900

    RDS Setting

--- mighty7/settings.py
+++ mighty7/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 import os
+import pymysql
+pymysql.install_as_MySQLdb()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,7 +30,8 @@ DEBUG = True
 
 ALLOWED_HOSTS = [
     ".ap-northeast-2.compute.amazonaws.com",
-    'mighty7.site'
+    'mighty7.site',
+    '127.0.0.1'
 ]
 
 
@@ -80,8 +83,15 @@ WSGI_APPLICATION = 'mighty7.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME' : 'TRAVEL', 
+        'USER' : 'root', 
+        'PASSWORD' : 'asd53014554!',
+        'HOST': 'db.cxeaa4sem1o5.ap-northeast-2.rds.amazonaws.com', 
+        'PORT': '3306',
+        'OPTIONS':{
+            'init_command' : "SET sql_mode='STRICT_TRANS_TABLES'"
+        }
     }
 }
 
